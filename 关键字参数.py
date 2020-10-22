917e2dcc84f0afd850b6baf62ede2baeffcd8a9f
str-horse <15681329@qq.com>
2020-10-22 17:45:59 +0800

    增加课后练习题

--- 关键字参数.py
+++ 关键字参数.py
@@ -9,3 +9,32 @@ def person(name, age, **kw):
 
 
 person('zhangsan',19,city = 'beijing',job = 'Engineer')
+
+
+def product(*args):
+    if args:
+        s = 1
+        for x in args:
+            s *= x
+        return s
+    else:
+        raise TypeError
+# 测试
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
