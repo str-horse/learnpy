249f1794f72e0930408eaa3f732b948fad23917c
str-horse <15681329@qq.com>
2020-10-22 23:05:57 +0800

    递归函数加入汉诺塔游戏

--- 递归求和.py
+++ 递归求和.py
@@ -5,13 +5,24 @@
 
 # print(get_sum(10))
 
+# def fibonacci(n):  # 递归法求第n个斐波那契数列
+#     if n == 1 or n == 2:
+#         return 1
+#     return fibonacci(n - 2) + fibonacci(n - 1)
 
-def fibonacci(n):  # 递归法求第n个斐波那契数列
-    if n == 1 or n == 2:
-        return 1
-    return fibonacci(n - 2) + fibonacci(n - 1)
+# m = int(input('请输入一个正整数：'))
+# print(fibonacci(m))
+
+
+def move(n, a, b, c):  # 汉诺塔游戏
+    if n == 1:
+        print(a, '->', c)
+    else:
+        move(n - 1, a, c, b)
+        print(a, '->', c)
+        move(n - 1, b, a, c)
+    return
 
 
 m = int(input('请输入一个正整数：'))
-print(fibonacci(m))
-
+move(m, 'A', 'B', 'C')
