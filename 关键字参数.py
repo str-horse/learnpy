f501e7cab60b38e87091f515cfbff441b42bc4e6
str-horse <15681329@qq.com>
2020-10-22 16:15:32 +0800

    关键字参数

---
+++ 关键字参数.py
@@ -0,0 +1,11 @@
+def person(name, age, **kw):
+    if 'city' in kw:
+        # 有city参数
+        pass
+    if 'job' in kw:
+        # 有job参数
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
+
+
+person('zhangsan',19,city = 'beijing',job = 'Engineer')
